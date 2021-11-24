f3a1c935ed5d137bda6bf35f996b6420a45a130f
Christopher Tregear <christregear@gmail.com>
2021-11-24 16:46:16 +0000

    Add humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error) {
             callback('Unable to find location.', undefined)
         } else {
-            callback(undefined, "It is currently " + body.current.temperature + ' degrees out. There is a '+ body.current.precip + ' % chance of rain.')
+            console.log(body.current.humidity)
+            callback(undefined, "It is currently " + body.current.temperature + ' degrees out. There is a '+ body.current.precip + ' % chance of rain.' + '\nThe humidity is: ' + body.current.humidity)
         }
     })
 }
